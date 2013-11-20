cfa3a54fee996da595c20985fddd4deff43464f1
Sam Nguyen <sam.nguyen@sendgrid.com>
2013-11-20 12:29:24 -0800

    Refactor

--- src/latencyapp/latencyapp.go
+++ src/latencyapp/latencyapp.go
@@ -26,53 +26,65 @@ type UrlList struct {
 	Urls []Url `json:"urls"`
 }
 
-// Params:
-// * code: status code to return (optional). If not provided, status code will
-//         be generated randomly
-// * delay: number of seconds to wait before returning response. Will be random
-// 		    if not provided.
-func handler(w http.ResponseWriter, r *http.Request) {
-	var delay int64
-	message := Message{Delay: delay, Path: r.URL.Path}
-
-	log.Printf("%s %s", r.Method, r.URL.String())
-
-	// get the number of seconds to wait to respond
-	latency := r.URL.Query().Get("delay")
-	seconds, err := strconv.Atoi(latency)
-
-	if err != nil || seconds < 0 {
-		delay = rand.Int63n(10)
+func pickDelayTime(delayStr string) (delay time.Duration) {
+	seconds, err := strconv.Atoi(delayStr)
+	if err != nil && delayStr != "" {
+		log.Printf("Invalid delay argument '%s': %s", delayStr, err)
+		delay = time.Duration(rand.Int63n(10))
+	} else if seconds < 0 {
+		delay = time.Duration(rand.Int63n(10))
 	} else {
-		delay = int64(seconds)
+		delay = time.Duration(seconds)
 	}
 
-	log.Printf("Going to wait %d seconds...\n", delay)
-	time.Sleep(time.Duration(delay) * time.Second)
+	delay *= time.Second
+	return delay
+}
 
-	var code int
-	var rnd int
-	codeStr := r.URL.Query().Get("code")
-
+func pickStatusCode(codeStr string) (code int) {
 	// Use the code we got passed in
-	if err == nil && codeStr != "" {
+	var err error
+	if codeStr != "" {
 		code, err = strconv.Atoi(codeStr)
-		if err != nil {
+		if err != nil && codeStr != "" {
+			log.Printf("Invalid code argument '%s': %s", codeStr, err)
 			code = 200
 		}
 
 		// Pick a random code
 	} else {
-		rnd = rand.Intn(10)
+		rnd := rand.Intn(10)
 		if rnd == 4 {
 			code = 400
 		} else if rnd == 5 {
 			code = 500
 		} else {
-			message.Message = "success"
 			code = 200
 		}
 	}
+
+	return code
+}
+
+// Params:
+// * code: status code to return (optional). If not provided, status code will
+//         be generated randomly
+// * delay: number of seconds to wait before returning response. Will be random
+// 		    if not provided.
+//
+// TODO: check Accept header and return JSON or text/plain (URL per line)
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s %s", r.Method, r.URL.String())
+
+	// get the number of seconds to wait to respond
+	delay := pickDelayTime(r.URL.Query().Get("delay"))
+
+	log.Printf("Going to wait %d seconds...\n", delay)
+	time.Sleep(delay)
+
+	message := Message{Delay: int64(delay.Seconds()), Path: r.URL.Path}
+
+	code := pickStatusCode(r.URL.Query().Get("code"))
 
 	if code >= 500 {
 		message.Message = "server error"
@@ -90,9 +102,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	log.Printf("Sending status code %d", code)
-	w.WriteHeader(code)
+	log.Printf("Sending %d: %s", code, jsonMessage)
 
+	w.WriteHeader(code)
 	fmt.Fprintf(w, "%s", jsonMessage)
 }
 
